461f5854e0445c224acbc2b7878c7f0e4e1ac87d
Taha Zaheer Khan <31067691+musaafir101@users.noreply.github.com>
2025-11-18 15:28:20 +0500

    ui updated

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
-export { default as Icon } from './Icon';
-export type { IconName } from './Icon';
-export { default as IconShowcase } from './IconShowcase';
-export { default as ContentInput } from './ContentInput';
-export { default as ImageUploader } from './ImageUploader';
-export { default as Terms } from './Terms';
-export { default as Support } from './Support';
-export { default as FAQ } from './FAQ';
+export { default as Icon } from "./Icon";
+export type { IconName } from "./Icon";
+export { default as IconShowcase } from "./IconShowcase";
+export { ContentInput as ContentInput } from "./ContentInput";
+export { ImageUploader as ImageUploader } from "./ImageUploader";
+export { default as Terms } from "./Terms";
+export { default as Support } from "./Support";
+export { default as FAQ } from "./FAQ";
