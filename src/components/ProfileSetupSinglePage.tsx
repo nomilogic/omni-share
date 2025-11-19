89f2777f69829e1c7e2daecf9452d267c39fec51
Taha Zaheer Khan <31067691+musaafir101@users.noreply.github.com>
2025-11-19 18:58:07 +0500

    fixed api update password

--- src/components/ProfileSetupSinglePage.tsx
+++ src/components/ProfileSetupSinglePage.tsx
@@ -511,18 +511,20 @@ const ProfileSetupSinglePage: React.FC = () => {
   return (
     <div className="bg-transparent">
       <div className=" flex flex-col md:flex-row-reverse jusitify-between items-between w-full p-4">
-        <button
-          onClick={handleSkip}
-          className="flex items-center gap-2 text-[#7650e3] hover:text-[#6540cc] font-semibold  transition-colors w-full justify-end text-sm hover:underline"
-        >
-          <ArrowLeft className="w-5 h-5" />
-          Back to Dashboard
-        </button>
         <div className="w-full">
-          <h1 className="text-3xl font-bold text-gray-900 mb-2">
-            Complete Your Profile
-          </h1>
-          <p className="text-gray-600">
+          <div className="flex justify-between items-center gap-2 mb-2">
+            <h1 className="text-3xl font-bold text-black  w-full">
+              Complete Your Profile
+            </h1>
+            <button
+              onClick={handleSkip}
+              className="flex  gap-2 top-5 text-[#7650e3] hover:text-[#6540cc] font-semibold transition-colors w-full justify-end text-sm hover:underline"
+            >
+              <ArrowLeft className="w-5 h-5" />
+              Back to Dashboard
+            </button>
+          </div>
+          <p className="text-gray-500">
             Tell us about yourself to personalize your experience
           </p>
         </div>
@@ -530,7 +532,6 @@ const ProfileSetupSinglePage: React.FC = () => {
       <div className="w-full max-w-5xl  mx-auto">
         <div className="bg-transparent  overflow-hidden relative">
           {/* Header */}
-    
 
           <div className="p-4">
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
