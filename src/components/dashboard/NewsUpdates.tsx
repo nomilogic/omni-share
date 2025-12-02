18ea23877be265103be116118674f1b1cf30fabb
Taha Zaheer Khan <31067691+musaafir101@users.noreply.github.com>
2025-12-02 19:52:03 +0500

    done

--- src/components/dashboard/NewsUpdates.tsx
+++ src/components/dashboard/NewsUpdates.tsx
@@ -1,22 +1,46 @@
-import { useState } from "react";
-import laptop from "../../assets/laptop.png";
+import { useState, useEffect } from "react";
+import laptop from "../../assets/dashboard-1.svg";
+import mobile from "../../assets/dashboard-2.svg";
+import tablet from "../../assets/dashboard-3.svg";
 
 function NewsUpdates() {
+  const slides = [
+    {
+      image: mobile,
+      text: "We’re also introducing a Draft Posts option, allowing you to create and save posts to publish later.",
+    },
+    {
+      image: laptop,
+      text: "We’ll soon be launching our Scheduled Post & Announcement feature — stay tuned for the official release!",
+    },
+    {
+      image: tablet,
+      text: "Create Teams & Manage Access Permissions — Coming Soon",
+    },
+  ];
+
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  return (
-    <div className="bg-gray-100 rounded-md  p-5  flex flex-col w-full h-full ">
-      <div className="flex-1  flex flex-col">
-        <img src={laptop} alt="laptop" />
+  // Auto-scroll effect
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % slides.length);
+    }, 3000); // change slide every 3 seconds
 
-        <p className="text-xs text-black ">
-          We'll soon be launching our Scheduled Post & Announcement feature –
-          stay tuned for the official release!
+    return () => clearInterval(interval); // cleanup on unmount
+  }, [slides.length]);
+
+  return (
+    <div className="bg-gray-100 rounded-md p-5 flex flex-col w-full h-full">
+      <div className="flex-1 flex flex-col items-center">
+        <img src={slides[currentSlide].image} alt="slide" className="" />
+        <p className="text-sm text-black text-center mt-5">
+          {slides[currentSlide].text}
         </p>
       </div>
 
       <div className="flex mb-2 justify-center gap-2 mt-auto">
-        {[0, 1, 2].map((i) => (
+        {slides.map((_, i) => (
           <button
             key={i}
             onClick={() => setCurrentSlide(i)}
